f3de694ba713c6a8940d1c98b56f3aad33ff1c8f
BarboraKas <91083592+BarboraKas@users.noreply.github.com>
2021-12-04 16:09:40 +0100

    added search engine

--- src/app.js
+++ src/app.js
@@ -39,10 +39,19 @@ function getTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+function search(city) {
+  let apiKey = "98b36d0829d6e91f19ba540afdc162ac";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getTemperature);
 }
 
-let apiKey = "98b36d0829d6e91f19ba540afdc162ac";
-let city = "Madrid";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(getTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
